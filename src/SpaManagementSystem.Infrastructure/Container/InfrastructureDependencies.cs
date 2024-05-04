f644339f2ea6b76007f6515353cf84dfcfdef538
zml18x <zml18v@gmail.com>
2024-05-04 18:47:04 +0200

    - Integrated Entity Framework for database operations. - Set up PostgreSQL database connection.

---
+++ src/SpaManagementSystem.Infrastructure/Container/InfrastructureDependencies.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpaManagementSystem.Infrastructure.Data.Context;
+
+namespace SpaManagementSystem.Infrastructure.Container
+{
+    /// <summary>
+    /// Provides a centralized class for configuring essential infrastructure services required by the application.
+    /// This class includes configurations for dependency injection, database context, identity services, and JWT authentication.
+    /// </summary>
+    public static class InfrastructureDependencies
+    {
+        /// <summary>
+        /// Configures core services and dependencies for the entire application.
+        /// This method serves as the central setup function for registering various services like database contexts,
+        /// identity management, and authentication mechanisms.
+        /// </summary>
+        /// <param name="services">The collection of service descriptors for registering application services.</param>
+        /// <param name="configuration">The application-wide settings and configurations,
+        /// used for setting up specific services like database connections and JWT settings.</param>
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            ConfigureDatabase(services, configuration);
+        }
+        
+        /// <summary>
+        /// Sets up the database contexts for the application using PostgreSQL,
+        /// ensuring that all database interactions are configured.
+        /// </summary>
+        /// <param name="services">The collection of services where database contexts are added.</param>
+        /// <param name="configuration">The application settings containing database connection strings.</param>
+        private static void ConfigureDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SmsDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("SmsConnection"));
+            });
+        }
+    }
+}
---
+++ src/SpaManagementSystem.Infrastructure/Data/Context/SmsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpaManagementSystem.Infrastructure.Data.Context
+{
+    /// <summary>
+    /// Represents the database context for the SpaManagementSystem.
+    /// </summary>
+    public class SmsDbContext : DbContext
+    {
+        public SmsDbContext() { }
+        public SmsDbContext(DbContextOptions<SmsDbContext> options) : base(options) { }
+
+        
+        
+        /// <summary>
+        /// Configures the model schema and relationships for the database context.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder instance used to construct the model for the context.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("SMS");
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
