f6af69e031daeb6e6472e7d20290092fe712e9a1
zml18x <zml18v@gmail.com>
2024-05-05 00:02:37 +0200

    Implement basic CRUD repository pattern for TEntity

---
+++ src/SpaManagementSystem.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,51 @@
+﻿namespace SpaManagementSystem.Domain.Interfaces
+{
+    /// <summary>
+    /// Defines the standard operations to be performed on a repository for a specific type of entity.
+    /// This interface provides an abstraction over basic CRUD operations to ensure a decoupled design
+    /// and ease of unit testing by allowing different storage implementations.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity this repository will manage. The entity must be a class.</typeparam>
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Retrieves all entities asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation and contains the enumerable of entity.</returns>
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        
+        /// <summary>
+        /// Retrieves a single entity by its unique identifier asynchronously.
+        /// </summary>
+        /// <param name="entityId">The unique identifier of the entity to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation and contains the entity found,
+        /// or null if no entity is found.</returns>
+        public Task<TEntity?> GetByIdAsync(Guid entityId);
+
+        /// <summary>
+        /// Creates a new entity asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to add to the repository.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        public Task CreateAsync(TEntity entity);
+
+        /// <summary>
+        /// Updates an existing entity.
+        /// </summary>
+        /// <param name="entity">The entity to update. The entity must already exist in the repository.</param>
+        public void Update(TEntity entity);
+
+        /// <summary>
+        /// Deletes an entity from the repository.
+        /// </summary>
+        /// <param name="entity">The entity to delete. The entity must exist in the repository.</param>
+        public void Delete(TEntity entity);
+
+        /// <summary>
+        /// Saves all changes made in the context of the repository asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation,
+        /// typically completing when all changes have been committed to the data store.</returns>
+        public Task SaveChangesAsync();
+    }
+}
---
+++ src/SpaManagementSystem.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaManagementSystem.Domain.Interfaces;
+using SpaManagementSystem.Infrastructure.Data.Context;
+
+namespace SpaManagementSystem.Infrastructure.Repositories
+{
+    /// <summary>
+    /// A generic repository implementation that handles CRUD operations for the specified TEntity type
+    /// within the context of the Spa Management System.
+    /// This class provides a layer of abstraction over the Entity Framework Core operations,
+    /// thereby centralizing the data access logic.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity this repository manages, which must be a class.</typeparam>
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly SmsDbContext _context;
+
+
+
+        /// <summary>
+        /// Initializes a new instance of the Repository with the specified database context.
+        /// </summary>
+        /// <param name="context">The database context this repository will operate on,
+        /// managing the lifecycle of the TEntity instances.</param>
+        public Repository(SmsDbContext context)
+        {
+            _context = context;
+        }
+        
+        
+        
+        /// <inheritdoc/>
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+            => await _context.Set<TEntity>().ToListAsync();
+
+        /// <inheritdoc/>
+        public async Task<TEntity?> GetByIdAsync(Guid entityId)
+            => await _context.Set<TEntity>().FindAsync(entityId);
+
+        /// <inheritdoc/>
+        public async Task CreateAsync(TEntity entity)
+        {
+            CheckEntityIsNull(entity);
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        /// <inheritdoc/>
+        public void Update(TEntity entity)
+        {
+            CheckEntityIsNull(entity);
+            _context.Update(entity);
+        }
+
+        /// <inheritdoc/>
+        public void Delete(TEntity entity)
+        {
+            CheckEntityIsNull(entity);
+            _context.Remove(entity);
+        }
+
+        /// <inheritdoc/>
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+
+        /// <summary>
+        /// Helper method to check if the entity is null, ensuring that operations do not proceed with null entities.
+        /// </summary>
+        /// <param name="entity">The entity to check.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the entity is null,
+        /// indicating an attempt to operate on a non-existent object.</exception>
+        private void CheckEntityIsNull(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity), $"{nameof(entity)} cannot be null");
+        }
+    }
+}
