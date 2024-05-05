7a1b84d67dac6b7e73df016d247fe3f2604c0dd5
zml18x <zml18v@gmail.com>
2024-05-05 19:50:53 +0200

    Added migration for UserProfiles Table

---
+++ src/SpaManagementSystem.Infrastructure/Data/Migrations/20240505140104_AddedUserProfilesTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaManagementSystem.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserProfilesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "SMS");
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                schema: "SMS",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfiles",
+                schema: "SMS");
+        }
+    }
+}
