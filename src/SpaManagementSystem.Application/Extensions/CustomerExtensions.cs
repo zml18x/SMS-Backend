a15fa0d4182753f450d79f765fccaf9c54abede5
zml18x <zml18v@gmail.com>
2024-12-17 18:36:14 +0100

    - Improved customer status updates. - Added notes to CustomerDto

--- src/SpaManagementSystem.Application/Extensions/CustomerExtensions.cs
+++ src/SpaManagementSystem.Application/Extensions/CustomerExtensions.cs
@@ -5,13 +5,14 @@ namespace SpaManagementSystem.Application.Extensions;
 
 public static class CustomerExtensions
 {
-    public static bool HasChanges(this Customer existingEmployee, UpdateCustomerRequest request)
+    public static bool HasChanges(this Customer existingCustomer, UpdateCustomerRequest request)
     {
-        return existingEmployee.FirstName != request.FirstName ||
-               existingEmployee.LastName != request.LastName ||
-               existingEmployee.Gender != request.Gender||
-               existingEmployee.PhoneNumber != request.PhoneNumber ||
-               existingEmployee.Email != request.Email ||
-               existingEmployee.Notes != request.Notes;
+        return existingCustomer.FirstName != request.FirstName ||
+               existingCustomer.LastName != request.LastName ||
+               existingCustomer.Gender != request.Gender ||
+               existingCustomer.PhoneNumber != request.PhoneNumber ||
+               existingCustomer.Email != request.Email ||
+               existingCustomer.Notes != request.Notes ||
+               existingCustomer.IsActive != request.IsActive;
     }
 }
